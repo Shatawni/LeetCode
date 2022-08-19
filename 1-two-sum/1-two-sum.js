cfadcccfa50ed83618e38aba455e6fd1389d09db
Shatawni Wright <shatawni.johnson@gmail.com>
2022-08-19 14:33:53 -0400

    Time: 83 ms (86.29%), Space: 42.8 MB (44.50%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let numsObj = {}
+
+  for (let p = 0; p < nums.length; p++) {
+    let currentVal = numsObj[nums[p]]
+
+    if (currentVal >= 0) {
+      return [currentVal, p]
+    } else {
+      let numToFind = target - nums[p]
+      numsObj[numToFind] = p;
+    }
+  }
+  return null;
+}
